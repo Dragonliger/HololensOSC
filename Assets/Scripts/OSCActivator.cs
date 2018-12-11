69393fe1199078ba8c09c794eecc744769d3365d
DragonligerBrix <Dragonliger@users.noreply.github.com>
2018-12-10 23:37:55 -0800

    Works

---
+++ Assets/Scripts/OSCActivator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityOSC;
+
+public class OSCActivator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        OSCHandler.Instance.Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OSCHandler.Instance.UpdateLogs();
+        if (OSCHandler.Instance.Servers["Hololens"].log.Count != 0)
+        {
+            print(OSCHandler.Instance.Servers["Hololens"].log[OSCHandler.Instance.Servers["Hololens"].log.Count - 1]);
+        }
+        else
+        {
+            print("No messages");
+        }
+	}
+}
