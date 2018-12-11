0deabebf5a1958128d0578d3b212086e1b20505c
DragonligerBrix <Dragonliger@users.noreply.github.com>
2018-12-11 11:09:29 -0800

    ForHololens

--- Assets/Scripts/OSCActivator.cs
+++ Assets/Scripts/OSCActivator.cs
@@ -5,6 +5,9 @@ using UnityOSC;
 
 public class OSCActivator : MonoBehaviour {
 
+    public GameObject Cube;
+    public Material Color;
+
 	// Use this for initialization
 	void Start () {
         OSCHandler.Instance.Init();
@@ -15,11 +18,24 @@ public class OSCActivator : MonoBehaviour {
         OSCHandler.Instance.UpdateLogs();
         if (OSCHandler.Instance.Servers["Hololens"].log.Count != 0)
         {
-            print(OSCHandler.Instance.Servers["Hololens"].log[OSCHandler.Instance.Servers["Hololens"].log.Count - 1]);
+            int Value = OSCParseInt(OSCHandler.Instance.Servers["Hololens"].log[OSCHandler.Instance.Servers["Hololens"].log.Count - 1]);
+            print(Value);
+            if(Value == 3)
+            {
+                Cube.GetComponent<Renderer>().material = Color;
+            }
         }
         else
         {
             print("No messages");
         }
 	}
+
+    public int OSCParseInt(string message)
+    {
+        string[] messageParts = message.Split(' ');
+        //return int.Parse(messageParts[1]);
+        print(messageParts[1]);
+        return 3;
+    }
 }
